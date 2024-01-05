cdfd59a298c3590308e9ca54bc4ba094f6b29e5a
santhakumar <santhakumar3490@gmail.com>
2024-01-05 11:57:11 +0530

    php admin panel with curd operation with xampp and RestApi also

---
+++ api.php
@@ -0,0 +1,24 @@
+<?php
+    $con = mysqli_connect("localhost","root","","projects");
+    $response = array();
+    if($con){
+        $sql = "select * from users";
+        $result = mysqli_query($con,$sql);
+        if($result){
+            $i=0;
+            while($row = mysqli_fetch_assoc($result)){
+                $response[$i]['id'] = $row['id'];
+                $response[$i]['firstname'] = $row['firstname'];
+                $response[$i]['lastname'] = $row['lastname'];
+                $response[$i]['email'] = $row['email'];
+                $response[$i]['password'] = $row['password'];
+                $response[$i]['gender'] = $row['gender'];
+                $i++;
+            }
+            echo json_encode($response,JSON_PRETTY_PRINT);
+        }
+    }else{
+        echo "Database connection failed";
+    }
+
+?>
